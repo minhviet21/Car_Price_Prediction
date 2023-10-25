c3163a01f3ed04f0103473228125058743904eee
minhviet21 <vmdiioenathn@gmail.com>
2023-10-25 10:06:08 +0700

    update ten xe and gia xe

--- crawling/crawling/spiders/bonbanh.py
+++ crawling/crawling/spiders/bonbanh.py
@@ -16,6 +16,7 @@ class DetailSpider(scrapy.Spider):
         details = response.css("div.txt_input span.inp::text")
 
         yield {
+            "Tên xe" :  response.css("span b i::text").get(),
             "Năm sản xuất" :  details[0].get().strip(),
             "Tình trạng" :    details[1].get().strip(),
             "Số km đã đi" :   details[-8].get().strip(),
@@ -25,5 +26,6 @@ class DetailSpider(scrapy.Spider):
             "Động cơ" :       details[-4].get().strip(),
             "Màu ngoại thất": details[-3].get().strip(),
             "Màu nội thất":   details[-2].get().strip(),
-            "Dẫn động sau":   details[-1].get().strip()
+            "Dẫn động sau":   details[-1].get().strip(),
+            "Giá xe"  :  response.css("div.title h1::text").get().split("-")[-1].strip()
         }
